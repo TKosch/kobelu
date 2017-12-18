b3fa2a1917223bb0fc157121106a258dbdf4ba24
Thomas Kosch <thomaskosch90@gmail.com>
2017-12-18 17:21:18 +0100

    Touch support implemented. Needs manual calibration
    Former-commit-id: 335b2bdd05061dbdb19dce949a7fe0ef59041b33

--- KoBeLUAdmin/Backend/TouchManager.cs
+++ KoBeLUAdmin/Backend/TouchManager.cs
@@ -21,6 +21,7 @@ namespace KoBeLUAdmin.Backend
         Image<Gray, byte> touchRoi;
         Rectangle roi;
 
+
         public TouchManager()
         {
             roi = new Rectangle(SettingsManager.Instance.Settings.SettingsTable.KinectDrawing.X, SettingsManager.Instance.Settings.SettingsTable.KinectDrawing.Y,
@@ -29,14 +30,33 @@ namespace KoBeLUAdmin.Backend
         }
 
 
-        public void DetectTouch(double pTouchDepthMin, double pTouchDepthMax)
+        public void DetectTouch(double pTouchDepthMin, double pTouchDepthMax, double pTouchMinArea = 50, double pTouchMaxArea = 55)
         {
             Image<Gray, byte> image = KinectManager.Instance.KinectConnector.DepthImgByte;
             touch = image.Cmp(pTouchDepthMin, Emgu.CV.CvEnum.CmpType.GreaterThan) & image.Cmp(pTouchDepthMax, Emgu.CV.CvEnum.CmpType.LessThan);
             touchRoi = touch;
             touchRoi.ROI = roi;
+            CvInvoke.FindContours(touchRoi, contours, null, Emgu.CV.CvEnum.RetrType.List, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
 
-            CvInvoke.Imshow("Test", touchRoi);
+            System.Drawing.PointF[] touchpoint_array = new System.Drawing.PointF[contours.Size];
+            touchPoints = new VectorOfPointF();
+
+            //CvInvoke.Imshow("Test", touchRoi);
+
+            for (int i = 0; i < contours.Size; i++)
+            {
+                if (CvInvoke.ContourArea(contours[i]) > pTouchMinArea)
+                {
+                    MCvScalar center = CvInvoke.Mean(contours[i]);
+                    touchpoint_array[i] = new System.Drawing.PointF((float)center.V0, (float)center.V1);
+                    touchPoints.Push(touchpoint_array);
+                }
+            }
+
+            for (int i = 0; i < touchPoints.Size; i++)
+            {
+                Console.WriteLine("Touch detected at position: " + touchPoints[i].X + " " + touchPoints[i].Y);
+            }
         }
 
     }

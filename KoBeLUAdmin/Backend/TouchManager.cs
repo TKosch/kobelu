4285a983863f0630f89cc20081aa14282d069e5b
Thomas Kosch <thomaskosch90@gmail.com>
2018-01-15 11:05:58 +0100

    Removed debug flags
    Former-commit-id: 8378028d56b3566d9fd7e3ad08a4b47257141d6b

--- KoBeLUAdmin/Backend/TouchManager.cs
+++ KoBeLUAdmin/Backend/TouchManager.cs
@@ -46,10 +46,10 @@ namespace KoBeLUAdmin.Backend
 
         public void DetectTouch(Image<Gray, Int32> pImage, Image<Gray, Int32> pReferenceImage, double pTouchDepthMin = 170, double pTouchDepthMax = 350, double pTouchMinArea = 5, double pTouchMaxArea = 15)
         {
-            
+            // TODO: check images sizes (exception is thrown when image size changes during runtime)
             mForeground = pReferenceImage - pImage;
             mTouch = mForeground.Cmp(pTouchDepthMin, Emgu.CV.CvEnum.CmpType.GreaterThan) & mForeground.Cmp(pTouchDepthMax, Emgu.CV.CvEnum.CmpType.LessThan);
-            CvInvoke.Imshow("test", mTouch);
+            //CvInvoke.Imshow("test", mTouch);
             CvInvoke.FindContours(mTouch, mContours, null, Emgu.CV.CvEnum.RetrType.List, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
             System.Drawing.PointF[] touchpoint_array = new System.Drawing.PointF[mContours.Size];
             TouchPoints = new VectorOfPointF();
@@ -63,6 +63,7 @@ namespace KoBeLUAdmin.Backend
                     double y = 1 - (touchpoint_array[i].Y / pImage.Height);
                     touchpoint_array[i].X = (float)x;
                     touchpoint_array[i].Y = (float)y;
+                    // DEBUG:
                     //Console.WriteLine("X: " + touchpoint_array[i].X + " Y: " + touchpoint_array[i].Y);
                 }
             }

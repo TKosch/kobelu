9325ff69562271a4f66b934de13614329ae7bf10
Thomas Kosch <thomaskosch90@gmail.com>
2018-03-01 11:32:42 +0100

    Added RealSense D415 without filters
    Former-commit-id: 40fc52e80f7374196291f039617237f114da8ff8

--- HciLab.Kinect/RealSenseManager.cs
+++ HciLab.Kinect/RealSenseManager.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -25,6 +26,11 @@ namespace HciLab.Kinect
         private Pipeline pipeline;
         private Colorizer colorizer;
         private CancellationTokenSource tokenSource = new CancellationTokenSource();
+        private const int MapDepthToByteImage = 8000 / 256;
+        private byte[] mColorPixels;
+        private byte[] mDepthPixels;
+        private const int SCALE_FACTOR = Int16.MaxValue / 1500;
+
 
         public static RealSenseManager Instance
         {
@@ -47,71 +53,98 @@ namespace HciLab.Kinect
         {
             try
             {
-                pipeline = new Pipeline();
-                colorizer = new Colorizer();
-
-                var cfg = new Config();
-                cfg.EnableStream(Stream.Depth, 1280, 720);
-                cfg.EnableStream(Stream.Color, Format.Rgb8);
-
-                pipeline.Start(cfg);
-
-                var token = tokenSource.Token;
-
-                var t = Task.Factory.StartNew(() =>
+                FrameQueue q = new FrameQueue();
+                using (var ctx = new Context())
                 {
-                    while (!token.IsCancellationRequested)
+                    var devices = ctx.QueryDevices();
+
+                    if (devices.Count == 0) return;
+                    var dev = devices[0];
+                    var depthSensor = dev.Sensors[0];
+
+                    pipeline = new Pipeline();
+                    colorizer = new Colorizer();
+                    var cfg = new Config();
+                    cfg.EnableStream(Stream.Depth, 1280, 720);
+                    cfg.EnableStream(Stream.Color, Format.Rgb8);
+
+                    pipeline.Start(cfg);
+
+                    var token = tokenSource.Token;
+
+                    var t = Task.Factory.StartNew(() =>
                     {
-                        var frames = pipeline.WaitForFrames();
-                        VideoFrame depthFrame = colorizer.Colorize(frames.DepthFrame);
-                        VideoFrame colorFrame = frames.ColorFrame;
+                        while (!token.IsCancellationRequested)
+                        {
+                            var frames = pipeline.WaitForFrames();
+                            VideoFrame depthFrame = frames.DepthFrame;
+                            VideoFrame colorFrame = frames.ColorFrame;
 
-                        if (depthFrame.Width == 0) return;
+                            if (depthFrame.Width == 0) return;
 
-                        byte[] byteDepthArray = new byte[depthFrame.Stride * depthFrame.Height];
-                        byte[] byteColorArray = new byte[colorFrame.Stride * colorFrame.Height];
-                        depthFrame.CopyTo(byteDepthArray);
-                        colorFrame.CopyTo(byteColorArray);
+                            mDepthPixels = new byte[depthFrame.Stride * depthFrame.Height * 2];
+                            mColorPixels = new byte[colorFrame.Width * colorFrame.Height * 3];
 
-                        BitmapSource bs_color = BitmapSource.Create(depthFrame.Width, depthFrame.Height,
-                            300, 300,
-                            System.Windows.Media.PixelFormats.Rgb24,
-                            null,
-                            byteColorArray,
-                            colorFrame.Stride);
+                            depthFrame.CopyTo(mDepthPixels);
+                            colorFrame.CopyTo(mColorPixels);
 
-                        BitmapSource bs_depth = BitmapSource.Create(depthFrame.Width, depthFrame.Height,
-                            300, 300,
-                            System.Windows.Media.PixelFormats.Rgb24,
-                            null,
-                            byteDepthArray,
-                            depthFrame.Stride);
 
-                        //// encode color stream
-                        //System.IO.MemoryStream outStreamColor = new System.IO.MemoryStream();
-                        //BitmapEncoder encoderColor = new BmpBitmapEncoder();
-                        //encoderColor.Frames.Add(BitmapFrame.Create(bs_color));
-                        //encoderColor.Save(outStreamColor);
-                        //Bitmap bmColor = new System.Drawing.Bitmap(outStreamColor);
-                        //Image<Bgra, byte> colorImage = new Image<Bgra, byte>(bmColor);
+                            // color frame
+                            Image<Rgb, byte> temp = new Image<Rgb, byte>(colorFrame.Width, colorFrame.Height);
+                            temp.Bytes = mColorPixels;
+                            Image<Bgra, byte> colorImage = new Image<Bgra, byte>(temp.Width, temp.Height);
+                            CvInvoke.cvCopy(temp.Convert<Bgra, byte>().Ptr, colorImage.Ptr, IntPtr.Zero);
 
-                        ////encode depth stream
-                        //System.IO.MemoryStream outStreamDepth = new System.IO.MemoryStream();
-                        //BitmapEncoder encoderDepth = new BmpBitmapEncoder();
-                        //encoderDepth.Frames.Add(BitmapFrame.Create(bs_depth));
-                        //encoderDepth.Save(outStreamDepth);
-                        //Bitmap bmDepth = new System.Drawing.Bitmap(outStreamDepth);
-                        //Image<Bgra, byte> depthImage = new Image<Bgra, byte>(bmDepth);
-                        //Image<Gray, byte> depthImageTemp = depthImage.Convert<Gray, byte>();
-                        //Image<Gray, int> depthImageTemp2 = depthImageTemp.Convert<Gray, int>();
+                            // depth frame
+                            ushort maxDepth = ushort.MaxValue;
+                            ProcessDepthFrameData(depthFrame.Data, (uint) (depthFrame.Width * depthFrame.Height), depthFrame.BitsPerPixel / 8, 0, maxDepth);
 
-                        //CameraManager.Instance.SetImages(colorImage, colorImage, depthImageTemp2, depthImageTemp2);
-                    }
-                }, token);
+                            int size = depthFrame.Width * depthFrame.Height * 2;
+                            byte[] managedArray = new byte[size];
+                            Marshal.Copy(depthFrame.Data, managedArray, 0, size);
+
+                            Image<Gray, Int16> temp2 = new Image<Gray, Int16>(depthFrame.Width, depthFrame.Height);
+                            temp2.Bytes = managedArray;
+
+                            temp2 = temp2.ConvertScale<Int16>(SCALE_FACTOR, 0);
+                            Image<Gray, int> depthImage = temp2.Convert<Gray, int>();
+
+                            CameraManager.Instance.SetImages(colorImage, colorImage, depthImage, depthImage);
+                        }
+                    }, token);
+                }
+
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Error during RealSense D415 startup");
+            }
+        }
+
+        /// <summary>
+        /// Directly accesses the underlying image buffer of the DepthFrame to 
+        /// create a displayable bitmap.
+        /// This function requires the /unsafe compiler option as we make use of direct
+        /// access to the native memory pointed to by the depthFrameData pointer.
+        /// </summary>
+        /// <param name="depthFrameData">Pointer to the DepthFrame image data</param>
+        /// <param name="depthFrameDataSize">Size of the DepthFrame image data</param>
+        /// <param name="minDepth">The minimum reliable depth value for the frame</param>
+        /// <param name="maxDepth">The maximum reliable depth value for the frame</param>
+        private unsafe void ProcessDepthFrameData(IntPtr depthFrameData, uint depthFrameDataSize, int bytesPerPixel, ushort minDepth, ushort maxDepth)
+        {
+            // depth frame data is a 16 bit value
+            ushort* frameData = (ushort*)depthFrameData;
+
+            // convert depth to a visual representation
+            for (int i = 0; i < (int)(depthFrameDataSize / bytesPerPixel); ++i)
+            {
+                // Get the depth for this pixel
+                ushort depth = frameData[i];
+
+                // To convert to a byte, we're mapping the depth value to the byte range.
+                // Values outside the reliable depth range are mapped to 0 (black).
+                this.mDepthPixels[i] = (byte)(depth >= minDepth && depth <= maxDepth ? (depth / MapDepthToByteImage) : 0);
             }
         }
 

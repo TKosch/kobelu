52d3065b90cc01c03e620a1a13858cb26b17fbe0
Thomas Kosch <thomaskosch90@gmail.com>
2018-08-24 13:29:55 +0200

    Added startup file
    Former-commit-id: b3c635d1561b9a008adc6624da4218c015955845

---
+++ startup.bat
@@ -0,0 +1,2 @@
+START /min C:\Users\kosch\workspace\KoBeLU-UID\Core\KoBeLUAdmin\bin\Debug\KoBeLUAdmin
+START C:\Users\kosch\workspace\KoBeLU-UID\GUI\build\UPDNetwork "mode=kiosk" "C:\Users\kosch\workspace\KoBeLU-UID\Core\KoBeLUAdmin\bin\Debug\workflows\guitest2.work"
